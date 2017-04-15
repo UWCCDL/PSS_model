cd13abfeb5062cb32f979f9bde538848ee235bb8
gardoma <andrea.stocco@gmail.com>
2017-04-15 09:43:10 -0700

    New simulations completed and zipped, correct values of ALPHA

---
+++ sims-competitive-alternative/sims-d1-d2-alpha-0.013-egs-0.1-block02-comp.lisp
@@ -0,0 +1,17 @@
+
+(load "/projects/actr/actr7/load-act-r.lisp")
+(load "../pss-device.lisp")
+(load "../pss-addition.lisp")
+(load "../pss-simulations.lisp")
+(defparameter *params* '((:alpha . 0.013) (:EGS . 0.1)))
+(with-open-file (out "sims-d1-d2-alpha-0.013-egs-0.1-block02-comp.txt" :direction :output 
+		     :if-exists :overwrite :if-does-not-exist :create)
+(simulate-d1-d2 "../pss-model-new-equation.lisp"
+                  250
+                  (seq 0.00 0.51 1/20)
+                  :d2-vals (seq 0.50 1.01 1/20)
+		  :stream out
+		  :report nil
+		  :utilities t
+		  :params *params*))
+(quit)
