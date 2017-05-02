bd5b79ef2aca69ab9306f48a34047046df2f7f1b
gardoma <andrea.stocco@gmail.com>
2017-05-01 20:34:59 -0700

    PD sims completed for non-competitive special model

--- sims-competitive-alternative/gen-pd.py
+++ sims-competitive-alternative/gen-pd.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 ## ---------------------------------------------------------------- ##
-## GEN-PD
+## GEN-GENETICS
 ## ---------------------------------------------------------------- ##
 ## This file generates a set of Lisp files, each of which runs a
 ## subset of the simulations that cover the entire parameter space
-## of d1 and d2 values in the range [0, 2] under the conditions that
-## maximize performance for the PD results (alpha = 0.018, egs = 0.1).
-## On multiprocessor architectures, each lisp file can be run as a
-## single thread, saving significant time.
+## of d1 and d2 in the range [0, 2] when the other parameters are
+## optimized to reproduce the genetics data (alpha = 0.018, egs = 0.1).
+## On multiprocessor machines, each file can be run independently.
 ## ------------------------------------------------------------------ ## 
 
 
@@ -16,10 +15,10 @@ TMPLT = """
 (load "../pss-device.lisp")
 (load "../pss-addition.lisp")
 (load "../pss-simulations.lisp")
-(defparameter *params* '((:alpha . 0.013) (:EGS . 0.1)))
-(with-open-file (out "sims-d1-d2-alpha-0.013-egs-0.1-block%02d-comp.txt" :direction :output 
+(defparameter *params* '((:alpha . 0.038) (:EGS . 0.1)))
+(with-open-file (out "sims-d1-d2-alpha-0.038-egs-0.1-block%02d-comp.txt" :direction :output 
 		     :if-exists :overwrite :if-does-not-exist :create)
-(simulate-d1-d2 "../pss-model-new-equation.lisp"
+  (simulate-d1-d2 "../pss-model-new-equation.lisp"
                   250
                   (seq %0.2f %0.2f 1/20)
                   :d2-vals (seq %0.2f %0.2f 1/20)
@@ -37,7 +36,7 @@ if __name__ == "__main__":
     i = 1
     for b1 in blocks:
         for b2 in blocks:
-            fout = open("sims-d1-d2-alpha-0.013-egs-0.1-block%02d-comp.lisp" % i, 'w')
+            fout = open("sims-d1-d2-alpha-0.038-egs-0.1-block%02d-comp.lisp" % i, 'w')
             s = TMPLT % (i, b1[0], b1[1] + 0.01, b2[0], b2[1] + 0.01)
             fout.write(s)
             fout.flush()
